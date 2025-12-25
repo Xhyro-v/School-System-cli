60a1f90efa4d831b3645d4d10b69f29c2a4580c5
Naufal_Azhar <naufalazharxyz12@gmail.com>
2025-12-25 12:12:22 +0700

    Main program logic

---
+++ DataManager/Main.py
@@ -0,0 +1,21 @@
+import json, os
+from Utility.Utility import Color, Backcol, Input, Font, Center
+from Datamanager.DataEditor import BaseManager, StudentDataEditor
+#from Datamanager.Graders import Base, BaseKit, CountScore, StudentFinalScore
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, "Datacenter")
+DATA_PATH = os.path.join(DATA_DIR, "Students.json")
+
+os.makedirs(DATA_DIR, exist_ok = True)
+
+print(Center.box(""))
+TM = input("Test add: ")
+if TM == "1":
+      SDE = StudentDataEditor(DATA_PATH)
+      name = Input.letter("nama: ").capitalize()
+      age = Input.number("umur: ").strip()
+      gender = input("jenis kelamin: ").capitalize()
+      nisn = Input.number("NISN murid: ").strip()
+      grade = Input.number("kelas: ").strip()
+      subgrade = Input.letter(f"{grade} ruang: \n")
+      SDE.add(grade, subgrade , name, age, gender, nisn)
