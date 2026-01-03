435108ecff7305a3603ba13c04ecd0e669475f49
Naufal_Azhar <naufalazharxyz12@gmail.com>
2026-01-03 09:13:18 +0700

    Refactor menu and input handling in Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 import os
-from Utility.Utility import Color, Input, Center, Font, Header
+from Utility.Utility import Color, Input, Center, Font, UI
 from Datamanager.DataEditor import StudentDataEditor
 from Datamanager.Graders import StudentScoreEditor
 
@@ -28,44 +28,55 @@ class Menu():
                 "Bahasa Daerah"
           ]
           
-          print(Center.box("Mini School System"))
-          print(Font.Italic(Center.text("By:Naufal\n")))
-          print("======Menu utama======")
-          print("1.Tambah siswa")
-          print("2.Masukan nilai siswa\n")
+          #print(Center.box("Mini School System"))
+          #print(Font.Italic(Center.text("By:Naufal\n")))
+          UI.opening("MINI SCHOOL SYSTEM")
+          print(Font.Italic(Center.text("Simple • Fast • Reliable")))
+          print(Font.Italic(Center.text("By:Naufal Azhar")))
+          print("")
+          print(UI.H1("\nMENU UTAMA"))
           while True:
+              print(Font.Bold("\n1.Tambah siswa"))
+              print(Font.Bold("2.Masukan nilai siswa"))
+              print(Font.Bold("3.Keluar\n"))
               TM = input("Pilih menu: ").strip().capitalize()
           
               if TM in ["1","Tambah siswa"]:
-                  print(Header.H1("\n\nTAMBAH SISWA"))
+                  print(UI.H1("\nTAMBAH SISWA"))
+         
                   SDE = StudentDataEditor(DATA_PATH)
           
                   name = Input.letter(f"{Color.Yellow('[1/6]')} Nama: ").capitalize()
-                  age = Input.number("[2/6] Umur: ").strip()
-                  gender = Input.letter("[3/6] Jenis kelamin: ").capitalize()
-                  while True:
-                     if gender not in ["Laki laki","Perempuan"]:
-                          gender = Input.letter("[3/6] Jenis kelamin: ").capitalize()
-                     else:
-                          nisn = Input.number("[4/6] NISN murid: ").strip()
-                          grade = Input.number("[5/6] Kelas: ").strip()
-                          subgrade = Input.letter(f"[6/6] {grade} Ruang: \n")
+                  age = Input.number(f"{Color.Yellow('[2/6]')} Umur: ").strip()
+                  gender = Input.letter(f"{Color.Yellow('[3/6]')} Jenis kelamin: ").capitalize()
+                  if gender not in ["laki laki","perempuan","Laki laki","Perempuan"]:
+                       while True:
+                           gender = Input.letter(f"{Color.Yellow('[3/6]')} Jenis kelamin: ").capitalize()
+                  nisn = Input.number(f"{Color.Yellow('[4/6]')} NISN murid: ").strip()
+                  grade = Input.number(f"{Color.Yellow('[5/6]')} Kelas: ").strip()
+                  print("""RUANG Kelas
+                  A     E
+                  B     F
+                  C     G
+                  D     H
+                          """)
+                  subgrade = Input.letter(f"{Color.Yellow('[6/6]')} {grade} Ruang: ")
+          
+                  SDE.add(grade, subgrade, name, age, gender, nisn)
+          
+                  SSE = StudentScoreEditor(DATA_PATH1)
+                  SSE.db["StudentScore"].setdefault(nisn, {})
+                  SSE.save()
                   
-                          SDE.add(grade, subgrade, name, age, gender, nisn)
-                  
-                          SSE = StudentScoreEditor(DATA_PATH1)
-                          SSE.db["StudentScore"].setdefault(nisn, {})
-                          SSE.save()
-                  
-                          print(Center.text("Done"))
           
               elif TM in ["2","Masukan nilai siswa"]:
+                  print(UI.H1("NILAI SISWA"))
                   SS = StudentScoreEditor.SelectSubject
                   IS = StudentScoreEditor.InputScores
                   R  = StudentScoreEditor.Report
           
-                  print(Font.Bold(Center.text("Memasukan nilai siswa")))
-                  nisn = Input.number("Masukkan NISN siswa: ").strip()
+                  nisn = Input.number(f"{Color.Yellow('[1/4]')}Masukkan NISN siswa: ").strip()
+                  print("")
                   subject = SS(daftar)
                   NH, PTS, PAS = IS()
                   Final = R(NH, PTS, PAS)
@@ -73,6 +84,7 @@ class Menu():
                   SSE = StudentScoreEditor(DATA_PATH1)
                   SSE.AddSubjectScore(nisn, subject, NH, PTS, PAS, Final)
           
+                  print("")
                   print(Color.Green(Center.text(
                       f"Nilai {subject} {nisn} berhasil disimpan"
                   )))
@@ -82,4 +94,4 @@ class Menu():
                   break
 
 Menu.MenuUtama()
-
+            
